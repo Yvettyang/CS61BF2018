532b50094759726318fd48bc17f8edd23aefa19b
Yvette Yang <Yvettyang@163.com>
2018-10-14 20:33:25 +0800

    lec2

---
+++ CS61BF2018/lec2/src/Primes.java
@@ -0,0 +1,54 @@
+public class Primes {
+
+    /** Print all primes up to ARGS[0] (interpreted as an
+     *  integer), 10 to a line. */
+    public static void main(String[] args) {
+        int testnum = 101;
+        printPrimes (testnum);
+    }
+
+    /** Print all primes up to and including LIMIT, 10 to
+     *  a line. */
+    public static void printPrimes (int limit) {
+        printPrimes (2, limit, 0);
+        System.out.println ();
+    }
+
+    /** Print all primes from L to U, inclusive, 10 to a
+     *  line, given that there are initially M primes
+     *  printed on the current line. */
+    public static void printPrimes (int L, int U, int M) {
+        if (L <= U) {
+            if (! isPrime (L))
+                printPrimes (L+1, U, M);
+            else {
+                if (M == 10) {
+                    System.out.println ();
+                    M = 0;
+                }
+                System.out.print (L + " ");
+                printPrimes (L+1, U, M+1);
+            }
+        }
+    }
+
+    /** True iff X is a prime number. */
+    public static boolean isPrime (int x) {
+        if (x <= 1)
+            return false;
+        else
+            return ! isDivisible (x, 2);
+    }
+
+    /** True iff X is divisible by any positive number >=K
+     *  and <X, given K>1. */
+    public static boolean isDivisible (int x, int k) {
+        if (k >= x)
+            return false;
+        else if (x % k == 0)
+            return true;
+        else
+            return isDivisible (x, k+1);
+    }
+
+}
